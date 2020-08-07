8acb17fbcdb84394705f6495369a61e6b4ba7806
Mithil Bhoras <mithil.bhoras@tapclicks.com>
2020-08-08 02:51:40 +0530

    Third Commit - Templates, Delete route

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, render_template, make_response
+from flask import Flask, request, render_template, make_response, redirect, Blueprint, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow, fields, Schema
 from flask_restful import Resource, Api
 from flask_wtf import FlaskForm
 from wtforms_sqlalchemy.fields import QuerySelectField
+from wtforms import StringField, FileField, validators
+from werkzeug.utils import secure_filename
 import os 
 
 app = Flask(__name__)
@@ -14,6 +16,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'songs.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'secret'
+app.config['UPLOAD_FOLDER'] = os.path.join(basedir,'songs')
+
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
@@ -53,9 +57,10 @@ def all_albums():
     return Album.query
 
 class AddSongForm(FlaskForm):
-    title = StringField()
+    title = StringField('Song name')
     artists = QuerySelectField(query_factory=all_artists, get_label='name')
     albums = QuerySelectField(query_factory=all_albums)
+    file = FileField()
 
 class ArtistSchema(Schema):
     id = fields.fields.Int()
@@ -70,85 +75,52 @@ class SongSchema(Schema):
     title = fields.fields.String()
     artist = fields.fields.Nested(ArtistSchema)
     album = fields.fields.Nested(AlbumSchema)
+    destination = fields.fields.String()
     
 song_schema = SongSchema()
 artist_schema = ArtistSchema()
 album_schema = AlbumSchema()
 songs_schema = SongSchema(many=True)
 
-class SongListResource(Resource):
-    def get(self):
-        songs = Song.query.all()
-        return make_response(render_template('show_all.html', songs = songs_schema.dump(songs)), 200)
-
-    def post(self):
-        new_song = Song(
-            title=request.form['title'],
-            artist=Artist.query.filter_by(id=request.form['artist']).first(),
-            album=Album.query.filter_by(id=request.form['album']).first()
-        )
-        db.session.add(new_song)
-        db.session.commit()
-        return song_schema.dump(new_song)
-
-class SongResource(Resource):
-    def get(self, song_id):
-        song = Song.query.get(song_id)
-        return song_schema.dump(song)
-
-    def delete(self, song_id):
-        Song.query.filter_by(id=song_id).delete()
-        db.session.commit()
-        return {}
-
-    def put(self, song_id):
-        song = Song.query.filter_by(id=song_id).first()
-        if song is not None:
-            for key, value in request.form.items():
-                if key == 'title':
-                    song.title = value
-                elif key == 'artist':
-                    song.artist = Artist.query.filter_by(id=request.form['artist']).first()
-                elif key == 'album':
-                    song.album = Album.query.filter_by(id=request.form['album']).first()
-            db.session.commit()
-        return song_schema.dump(song)
-
-class ArtistListResource(Resource):
-    def post(self):
-        new_artist = Artist(
-            name=request.form['name'],
-        )
-        db.session.add(new_artist)
-        db.session.commit()
-        return artist_schema.dump(new_artist)
-
-class AlbumListResource(Resource):
-    def post(self):
-        new_album = Album(
-            name=request.form['name'],
-        )
-        db.session.add(new_album)
-        db.session.commit()
-        return album_schema.dump(new_album)
-
-# api.add_resource(SongListResource, '/songs')
-# api.add_resource(ArtistListResource, '/artists')
-# api.add_resource(AlbumListResource, '/albums')
-# api.add_resource(SongResource, '/song/<string:song_id>')
-
-@app.route('/songs',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def songs():
     if request.method == 'GET':
         songs = Song.query.all() 
         return render_template('show_all.html', songs = songs_schema.dump(songs))
 
-@app.route('/add_song',methods=['GET','POST'])
+@app.route('/song/add',methods=['GET','POST'])
 def add_song():
     form = AddSongForm()
     if form.validate_on_submit():
-        return '{}'.format(form.artists.data)
+
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+        new_song = Song(
+            title=form.title.data,
+            artist=form.artists.data,
+            album=form.albums.data,
+            destination = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        )
+        db.session.add(new_song)
+        db.session.commit()
+        return redirect('/')
     return render_template('add_song.html',form=form)
+
+@app.route('/song/delete/<id>', methods=['GET','DELETE'])
+def delete_song(id):
+    song = Song.query.get(id)
+    if not song:
+        flash('Song does not exist')
+        return redirect('/')
+    db.session.delete(song)
+    db.session.commit()
+    return redirect('/')
 
 if __name__ == '__main__':
    app.run(debug=True)
